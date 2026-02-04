492f1adaaff2f45036489f999d707c45f77df811
Fabien Moullec <fabien.moullec50@gmail.com>
2026-02-04 15:08:32 +0100

    small edits

--- Keep_species_more_than_1percent_occ.r
+++ Keep_species_more_than_1percent_occ.r
@@ -1,17 +1,17 @@
-nocc <- apply(Y>0,2,sum)
-totabu <- apply(Y,2,sum)
-threshold = floor(nrow(Y)*0.01)
+nocc <- apply(Y > 0, 2, sum)
+totabu <- apply(Y, 2, sum)
+threshold <- floor(nrow(Y) * 0.01)
 
-#number of species kept
-N_kept_sp = sum(nocc>threshold)
+# Number of species kept
+N_kept_sp <- sum(nocc > threshold)
 print(paste0(N_kept_sp, " species will be retained"))
 
-#percentage of abundance conserved
-percent_abund_kept = sum(Y[,nocc>threshold])/sum(Y)*100
-print(paste0("The ", N_kept_sp, " species retained account for ", round(percent_abund_kept,2)," % of all abundances"))
+# Percentage of abundance conserved
+percent_abund_kept <- sum(Y[, nocc > threshold]) / sum(Y) * 100
+print(paste0("The ", N_kept_sp, " species retained account for ", round(percent_abund_kept,2)," % of all abundances")) # nolint
 
-#Select only species with occurrence in at least 1% of grid cells
-abu <- Y[,nocc>threshold]
+# Select only species with occurrence in at least 1% of grid cells
+abu <- Y[, nocc > threshold]
 
-selected_sp = colnames(abu)
+selected_sp <- colnames(abu)
 save(selected_sp, file = "Inputs_HMSC/selected_sp.RData")
